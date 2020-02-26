f1e75c24f8de32da0206d1f9045f503d5cdfe060
saarzeev <saarzeev@post.bgu.ac.il>
2020-02-26 18:27:29 +0200

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void getX() {
+        Main main = new Main();
+        assertEquals(10, main.getX());
+    }
+
+    @org.junit.Test
+    public void getX2() {
+        Main main = new Main();
+        main.setX(3);
+        assertEquals(10, main.getX());
+    }
+}
